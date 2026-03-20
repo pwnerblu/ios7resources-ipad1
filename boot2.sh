03081c9aaab72d17f7680ee4128aa87fd84b128e
pwnerblu <alexisjeanstansbury@gmail.com>
2026-03-19 20:31:15 -0400

    boot update

--- boot2.sh
+++ boot2.sh
@@ -9,7 +9,7 @@ $SCRIPT_DIR/bins/pzb -g kernelcache.release.n90 https://secure-appldnld.apple.co
 $SCRIPT_DIR/bins/xpwntool iBSS.n81ap.RELEASE.dfu iBSS.dec -iv daafc6ddd42c8f807000b9c1dd453236 -k 1e68d69064ca17c6717be4fa4ff09a71eba1ad0af2a96df4b99a69f6e7258058
 $SCRIPT_DIR/bins/xpwntool iBEC.n81ap.RELEASE.dfu iBEC.dec -iv fb44e5dbd3eb75d20f83c0f14d452346 -k 12a5192b4a2e860a76e9368e18e182e5f9f4809dcba62098fcbbaa63ef998a3c
 $SCRIPT_DIR/bins/iBoot32Patcher iBSS.dec iBSS.patched --rsa
-$SCRIPT_DIR/bins/iBoot32Patcher iBEC.dec iBEC.patched --rsa --ticket --debug -b "-v amfi=0xff amfi_get_out_of_my_way=1 cs_enforcement_disable=1 pio-error=0"
+$SCRIPT_DIR/bins/iBoot32Patcher iBEC.dec iBEC.patched --rsa --ticket --debug -b "-v pio-error=0"
 $SCRIPT_DIR/bins/img3maker -f iBSS.patched -o iBSS -t ibss
 $SCRIPT_DIR/bins/img3maker -f iBEC.patched -o iBEC -t ibec
 rm -rf iBSS.dec iBSS.patched iBEC.dec iBEC.patched
